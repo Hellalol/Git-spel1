2dd9ad0c3842f0f72eaa22a3f8836aa63b873ecc
Valle <valletsf@gmail.com>
2020-08-19 20:29:51 +0200

    Added two methods, createPlayers and findPlayer

---
+++ src/com/pillan/Player.java
@@ -0,0 +1,56 @@
+package com.pillan;
+
+public class Player {
+
+
+    private String name;
+    private long attempts;
+    private long score;
+    private long storedSips;
+    private boolean doubleUp;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getAttempts() {
+        return attempts;
+    }
+
+    public void setAttempts(long attempts) {
+        this.attempts = attempts;
+    }
+
+    public long getScore() {
+        return score;
+    }
+
+    public void setScore(long score) {
+        this.score = score;
+    }
+
+
+    public long getStoredSips() {
+        return storedSips;
+    }
+
+    public void setStoredSips(long storedSips) {
+        this.storedSips = storedSips;
+    }
+
+    public boolean isDoubleUp() {
+        return doubleUp;
+    }
+
+    public void setDoubleUp(boolean doubleUp) {
+        this.doubleUp = doubleUp;
+    }
+}
