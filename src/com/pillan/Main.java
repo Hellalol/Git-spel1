f988ca0fb54dd25bfa7d2eb1d5f7c93664507bc4
albin <albin.gudmundsson@gmail.com>
2020-08-20 15:23:44 +0200

    merged with gameBoardLogic

--- src/com/pillan/Main.java
+++ src/com/pillan/Main.java
@@ -3,8 +3,9 @@ package com.pillan;
 public class Main {
 
     public static void main(String[] args) {
-        //GameBoard g = new GameBoard();
-        Game g = new Game();
+        GameBoard g = new GameBoard();
+        PlayerList playerList = new PlayerList();
+        Game game = new Game(playerList);
 
     }
 }
